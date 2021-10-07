7295a1eb1b49ca05269977a56fa3dc93a1b28134
shddd <61633436+shddd@users.noreply.github.com>
2021-10-07 21:01:37 +0200

    Update tri à bulle.ml

--- tri à bulle.ml
+++ tri à bulle.ml
@@ -1,1 +1,24 @@
+(*ALPHA CODE A RETRAVAILLER*)
 
+
+
+
+
+(*
+tri_à_bulles(Tableau T)
+   pour i allant de (taille de T)-1 à 1
+       pour j allant de 0 à i-1
+           si T[j+1] < T[j]
+               (T[j+1], T[j]) = (T[j], T[j+1])*)
+
+let bubbleSort tab =
+	let n = Array.length tab in
+    for i = 1 to n-1 do
+    	for j = 0 to i-1 do
+        	if tab.(n -j + 1) < tab.(n -j) then
+            	let a = ref tab.(n -j) in
+                let b = ref tab.(n -j+1) in
+                tab.(n - j) <- !b;
+                tab.(n - j + 1) <- !a
+     done;done; 
+tab;;
