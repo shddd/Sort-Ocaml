e73e794ee7595ffa7245529f5c50995f837790bc
shddd <61633436+shddd@users.noreply.github.com>
2021-10-11 21:23:02 +0200

    Update tri par tas.ml

--- tri par tas.ml
+++ tri par tas.ml
@@ -1,1 +1,7 @@
-
+(*
+Tri par tas / heapsort
+Complexité en temps:Au mieux O(n), En moyenne O(n log n) Au pire O(n log n)
+Complexité en espace : Au pire O(1)
+Entrée : une liste A
+Sortie : A classée dans l'ordre croissant 
+*)
